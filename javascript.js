74dc1506c01c429a15db2586d9664474d29cd096
Jillian Werner <jillianlwerner@gmail.com>
2020-02-13 17:00:41 -0500

    Allow user to reset sketchbook; make hover color darken on subsequent passes

--- javascript.js
+++ javascript.js
@@ -1,17 +1,65 @@
 let outerContainer = document.querySelector('.container');
-let gridSize = 256;
+drawGrid();
 
-for (let i = 1; i <= gridSize; i++) {
-    outerContainer.appendChild(document.createElement("div"));
+function drawGrid(userSelection) {
+    let defaultGridSize = 16;
+    let gridSize = userSelection ** 2 || defaultGridSize ** 2;
+
+    outerContainer.style.gridTemplateColumns = `repeat(${userSelection || defaultGridSize}, 1fr)`;
+    outerContainer.style.gridTemplateRows = `repeat(${userSelection || defaultGridSize}, 1fr)`;
+
+    for (let i = 1; i <= gridSize; i++) {
+        outerContainer.appendChild(document.createElement("div"));
+    }
+
+    addColorChange();
 }
 
-let gridChildren = document.querySelector('.container').children;
-console.log(gridChildren);
+function addColorChange() {
+    outerContainer.addEventListener("mouseover", hoverOver);
 
-for (const child of gridChildren) {
-    child.addEventListener("mouseover", hoverOver);
+    function hoverOver(event) {
+        if (event.target !== outerContainer) {
+            event.target.style.backgroundColor = `rgba(0, 0, 0, ${getAlpha(event.target)}`;
+        }
+    }
 }
 
-function hoverOver(event) {
-    event.target.classList.add('hover-change');
+function getAlpha(element) {
+    // regex targeted at numbers with digits taken from this StackOverflow comment
+    // https://stackoverflow.com/questions/3751877/how-to-extract-r-g-b-a-values-from-css-color#comment79938383_3752026
+    let alphaTag = Number(window.getComputedStyle(element).backgroundColor.match(/[.?\d]+/g)[3]);
+
+    if (alphaTag <= 1) {
+        return alphaTag += 0.1;
+    }
+
+    return alphaTag;
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * (255 + 1));
+}
+
+let resetButton = document.querySelector('#reset-sketchbook');
+resetButton.addEventListener("click", resetSketchbook);
+
+function resetSketchbook() {
+    let gridChildren = document.querySelector('.container').children;
+
+    for (const child of gridChildren) {
+        child.style.backgroundColor = "";
+    }
+
+    drawGrid(requestGridSize());
+}
+
+function requestGridSize() {
+    userSelection = Number(prompt("How many rows would you like the sketchbook to have?"));
+
+    if (typeof userSelection === "number" && userSelection > 0) {
+        return userSelection;
+    } else {
+        requestGridSize();
+    } 
 }
