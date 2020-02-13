1e9eaffa0af4d58370e883abb847ebe8d561ade6
Jillian Werner <jillianlwerner@gmail.com>
2020-02-13 12:38:39 -0500

    Add eventListener to change div background color on hover

--- javascript.js
+++ javascript.js
@@ -4,3 +4,14 @@ let gridSize = 256;
 for (let i = 1; i <= gridSize; i++) {
     outerContainer.appendChild(document.createElement("div"));
 }
+
+let gridChildren = document.querySelector('.container').children;
+console.log(gridChildren);
+
+for (const child of gridChildren) {
+    child.addEventListener("mouseover", hoverOver);
+}
+
+function hoverOver(event) {
+    event.target.classList.add('hover-change');
+}
